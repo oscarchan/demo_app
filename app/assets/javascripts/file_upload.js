bdd59c54ab70d8e5eb784284f7463157a3fd8415
“Oscar <oscarchan@gmail.com>
2015-01-07 16:46:24 -0800

    force no js compression

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -1,4 +1,10 @@
 $(function() {
     // force cross domain issue on sub domain
     document.domain = document.domain.split(".").slice(1).join(".");
+
+    // ie8 hack
+    if (typeof console === "undefined" || typeof console.log === "undefined") {
+        console = {};
+        console.log = function() {};
+    }
 });
